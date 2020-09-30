f0f169e7ebb83ddfc3758ac59996935d6acdd4b9
Kaare Hattel <kaare.hattel@gmail.com>
2020-09-30 12:24:36 +0200

    script to find 1000 words from w2v model file containing the 1000 unique words of length > 3

---
+++ preprocess/generateWords.py
@@ -0,0 +1,12 @@
+import gensim
+from gensim.models import Word2Vec
+
+amount_of_words = 0
+
+model = Word2Vec.load('Tmp/winemodel')
+with open('Data/1000Words.txt', 'w') as f:
+    for word in model.wv.vocab:
+        if amount_of_words < 1000:
+            if len(word) > 3:
+                amount_of_words += 1
+                f.write(word+'\n')
