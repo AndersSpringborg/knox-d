1351779b8fab2fdf08f19e64d3abca4a954f1a41
Anders Aaen Springborg <aaspringborg@gmail.com>
2020-10-05 10:39:38 +0200

    We try again

---
+++ WordEmbeddings/word2vec_module.py
@@ -0,0 +1,33 @@
+from Preprocess.bigrams import bigramDetection
+from Preprocess.cleaner import cleaning
+import pandas as pd
+import multiprocessing
+from gensim.models import Word2Vec
+
+def createw2vmodel():
+    # Predefines
+    INPUT_FILE = 'Data/winemagClean.csv'
+
+    # Read the file
+    df = pd.read_csv(INPUT_FILE)
+
+    # Remove missing values if any -- OUTCOMMENT THIS FUNCTION CALL IF THE FILE IS NOT CLEANED
+    # cleaning(df)
+
+    # Create bigrams
+    sentences = bigramDetection(df)
+
+    # Train the model
+    cores = multiprocessing.cpu_count() # Count the number of cores in a computer
+    model = Word2Vec(min_count=2,
+                     window=10,
+                     size=50,
+                     sample=6e-5,
+                     alpha=0.03,
+                     min_alpha=0.0007,
+                     negative=20,
+                     workers=cores-1)
+
+    model.build_vocab(sentences, progress_per=10000)
+    model.train(sentences, total_examples=model.corpus_count, epochs=30, report_delay=1)
+    model.save('Models/winemodel')
--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-from w2v import *
-from testmodule import *
-import gensim.models
-import spacy
+import os
+import subprocess
+
+from gensim.scripts.glove2word2vec import glove2word2vec
+from gensim.test.utils import get_tmpfile
+
+from WordEmbeddings.word2vec_module import createw2vmodel
+from WordEmbeddings.spacy_module import createspacymodel
+from WordEmbeddings.glove_module import createglovemodel
+from Preprocess.bigrams import bigramDetection
+from Preprocess.cleaner import cleaning
+from Tests.test_module import findsimilarities
+from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
+import pandas as pd
 
 
-# Create new word2vec spacy_model:
-# createw2vmodel()
+# Create new models:
+#createw2vmodel()
+#createspacymodel()
+#createglovemodel()
+
 
 # Load models:
 w2v_model = Word2Vec.load('Models/winemodel')
-pre_model = gensim.models.KeyedVectors.load_word2vec_format('Models/GoogleNews-vectors-negative300.bin.gz', binary=True)
 
+#glove_file = 'GloveSetup/vectors.txt'
+#tmp_file = get_tmpfile('test_w2v.txt')
+#_ = glove2word2vec(glove_file, tmp_file)
+#glove_model = KeyedVectors.load_word2vec_format(tmp_file)
 
-# Run test on w2v spacy_model:
-test(pre_model)
+# Load the pre-trained model as KeyedVectors rather than a Word2Vec model, since it is simply a metrice of words and
+# their vector representations rather than a complete word2vec model!
+# pre_model = KeyedVectors.load('Models/premodel')
+
+model = w2v_model.wv
+
+print(model.word_vec('sweet'))
+
+# Run test on most similar results
+
+#w2v_score, glove_score = findsimilarities(w2v_model, glove_model, google_model)
+#print('--- WHICH MODEL IS BETTER?? google model ---')
+#enunmarator = (869 * 5)
+#print('w2v score:   ', w2v_score, " pct: ", w2v_score/enunmarator )
+#print('glove score: ', glove_score, " pct: ", glove_score/enunmarator)
