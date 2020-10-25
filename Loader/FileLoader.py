48fe1ec5d9cb849d9b12e6a50ac9116dffb27890
Kaare Hattel <kaare.hattel@gmail.com>
2020-10-25 13:16:54 +0100

    tests the fileloader.py

--- Loader/FileLoader.py
+++ Loader/FileLoader.py
@@ -1,8 +1,17 @@
+import os
+import sys
+
 from Resources.JsonWrapper import Content
 import json
 
 def load_json_file(path: str) -> Content:
-    with open(path, 'r', encoding='utf8') as file:
-        data = json.load(file)
-    return Content(data['properties']['content']['properties'])
+    try:
+        if os.stat(path).st_size > 0:
+            with open(path, 'r', encoding='utf8') as file:
+                data = json.load(file)
+            return Content(data['properties']['content']['properties'])
+        else:
+            raise IOError
+    except FileNotFoundError as e:
+        raise e
 
--- test/test_loader.py
+++ test/test_loader.py
@@ -1,98 +1,122 @@
 import pytest
 from Loader.FileLoader import load_json_file
-from Resources.JsonWrapper import Content, Section, Paragraph
+import json
+
 
 class TestLoader:
 
     def setup_method(self):
-        pass
+        self.json_file_path = '../TestData/manual_test_file.json'
+
+    def test_json_load_returns_dict(self):
+        # Arrange
+
+        # Act
+        with open(self.json_file_path, 'r', encoding='utf8') as file:
+            result = json.load(file)
+
+        # Assert
+        isinstance(result, dict)
+
+    def test_load_json_file_throws_exception_if_invalid_path(self):
+        # Arrange
+        invalid_path = 'some invalid path'
+        expected_error = FileNotFoundError
+
+        # Act
+
+        # Assert
+        with pytest.raises(expected_error):
+            assert load_json_file(invalid_path)
+
+    def test_load_json_file_throws_exeption_if_json_file_is_empty(self):
+        # Arrange
+        empty_file_path = "../TestData/empty_test_file.json"
+        expected_error = IOError
+
+        # Act
+
+        # Assert
+        with pytest.raises(expected_error):
+            assert load_json_file(empty_file_path)
 
     def test_assign_correct_publisher_to_content(self):
         # Arrange
-        json_file_path = '../TestData/manual_test_file.json'
+        expected = {'type': 'string'}
 
         # Act
-        result = load_json_file(json_file_path)
+        result = load_json_file(self.json_file_path)
 
         # Assert
-        assert result.publisher == {'type': 'string'}
+        assert result.publisher == expected
 
-    def test_assign_correct_publishedAt_to_content(self):
+    def test_assign_correct_published_at_to_content(self):
         # Arrange
-        json_file_path = '../TestData/manual_test_file.json'
+        expected = {'type': 'string'}
 
         # Act
-        result = load_json_file(json_file_path)
-        print(result.publisher)
+        result = load_json_file(self.json_file_path)
 
         # Assert
-        assert result.publishedAt == {'type': 'string'}
+        assert result.publishedAt == expected
 
     def test_assign_correct_title_to_content(self):
         # Arrange
-        json_file_path = '../TestData/manual_test_file.json'
-
+        expected = {'type': 'string'}
 
         # Act
-        result = load_json_file(json_file_path)
+        result = load_json_file(self.json_file_path)
         print(result.publisher)
 
         # Assert
-        assert result.title == {'type': 'string'}
+        assert result.title == expected
 
     def test_can_extract_sections_to_content(self):
         # Arrange
-        json_file_path = '../TestData/manual_test_file.json'
+        expected = {'type': 'string'}
 
         # Act
-        result = load_json_file(json_file_path)
-        print(result.publisher)
+        result = load_json_file(self.json_file_path)
 
         # Assert
         assert result.sections is not None
 
-
-
-
     def test_assign_correct_page_to_section(self):
         # Arrange
-        json_file_path = '../TestData/manual_test_file.json'
+        expected = {'type': 'string'}
 
         # Act
-        result = load_json_file(json_file_path)
+        result = load_json_file(self.json_file_path)
 
         # Assert
-        assert (result.sections[0].page == {'type': 'string'})
-
+        assert (result.sections[0].page == expected)
 
     def test_assign_correct_header_to_section(self):
         # Arrange
-        json_file_path = '../TestData/manual_test_file.json'
+        expected = {'type': 'string'}
 
         # Act
-        result = load_json_file(json_file_path)
+        result = load_json_file(self.json_file_path)
 
         # Assert
-        assert (result.sections[0].header == {'type': 'string'})
-
+        assert (result.sections[0].header == expected)
 
     def test_assign_correct_page_to_paragraph(self):
         # Arrange
-        json_file_path = '../TestData/manual_test_file.json'
+        expected = {'type': 'string'}
 
         # Act
-        result = load_json_file(json_file_path)
+        result = load_json_file(self.json_file_path)
 
         # Assert
-        assert (result.sections[0].paragraphs[0].page == {'type': 'string'})
-
+        assert (result.sections[0].paragraphs[0].page == expected)
 
     def test_assign_correct_text_to_paragraph(self):
         # Arrange
-        json_file_path = '../TestData/manual_test_file.json'
+        expected = {'type': 'string'}
 
         # Act
-        result = load_json_file(json_file_path)
+        result = load_json_file(self.json_file_path)
 
         # Assert
-        assert (result.sections[0].paragraphs[0].text == {'type': 'string'})
+        assert (result.sections[0].paragraphs[0].text == expected)
