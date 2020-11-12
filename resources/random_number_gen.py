f052cb2201f61b00464a1993636e7888b6eb0a45
Kasper Stenholt Winkler <44102455+KasperSWinkler@users.noreply.github.com>
2020-11-12 14:42:37 +0100

    Added cleaner print statements for loading

--- resources/random_number_gen.py
+++ resources/random_number_gen.py
@@ -5,9 +5,15 @@ def random_percentage_count():
     This is gimmick percentages for fun loading experience!!!
     """
     var = "%"
-    listofnumbers = random.sample(range(0, 99), 5)
+    forloopnumber = random.randint(1,10)
+    listofnumbers = random.sample(range(0, 99), forloopnumber)
+    if 69 not in listofnumbers:
+        listofnumbers.append(69)
     listofnumbers.sort()
 
     for number in listofnumbers:
         print(number, var, sep='')
-    print("100%")
+    if listofnumbers[0] < 5:
+        print("420%")
+    else:
+        print("100%")
