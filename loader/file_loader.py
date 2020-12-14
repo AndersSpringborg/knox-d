493ff835fa80df3bedff8d4b725bb9c63a69d489
Anders Aaen Springborg <aaspringborg@gmail.com>
2020-12-14 16:32:24 +0100

    remove old load json

--- loader/file_loader.py
+++ loader/file_loader.py
@@ -1,19 +1,5 @@
-import os
 import json
 from resources.json_wrapper import Content, Manual
-
-
-def load_json_file_into_content_object(path: str) -> Content:
-    """
-    Loads a json file into a Content object.
-    """
-    if os.stat(path).st_size > 0:
-        with open(path, 'r', encoding='utf8') as file:
-            data = json.load(file)
-
-        return Content(data['properties']['content']['properties'])
-
-    raise IOError
 
 
 def load_json(io_stream):
