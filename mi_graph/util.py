9eaa08e3eac092974b9153bfc3a4f986a3129833
Anders Aaen Springborg <aaspringborg@gmail.com>
2020-12-16 15:33:11 +0100

    new argsparser for flask

--- mi_graph/util.py
+++ mi_graph/util.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 import os
 import sys
 from argparse import ArgumentParser
@@ -8,21 +8,24 @@ from spacy.cli import download
 
 from loader.file_loader import load_json
 from mi_graph import api, pipeline
+from resources.error import error
+
+MODE_FILE = 'file'
+MODE_SERVE = 'flask'
 
 
-def setup_parser(_parser: ArgumentParser) -> ArgumentParser:
-    _parser.prog = "mi-graph"
+def setup_parser(parser: ArgumentParser) -> ArgumentParser:
+    parser.prog = "mi-graph"
 
-    _parser.add_argument("file", help="Please indicate the json file you want to process.")
+    parser.add_argument("mode", choices=[MODE_FILE, MODE_SERVE],
+                        help='Choose if you want to process a file, or run the program as a rest api')
+    parser.add_argument("--file", "-f", help="Please indicate the json file you want to process.",
+                        metavar='')  # makes help prettier
 
-    _parser.add_argument("--visualisation", "-v", action="store_true", default=False,
-                         help="This option visualizes the graph with plotly, "
-                              "after the script has run")
-
-    _parser.add_argument("--serve", "-s", action="store_true",
-                         help="This option runs mi_graph as an API")
-
-    return _parser
+    parser.add_argument("--visualisation", "-v", action="store_true", default=False,
+                        help="This option visualizes the graph with plotly, "
+                             "after the script has run")
+    return parser
 
 
 def make_path(path):
@@ -52,23 +55,26 @@ def ensure_models_installed():
 def cli():
     # Setup the parser and parse the arguments
     parser = setup_parser(ArgumentParser())
-    try:
-        args = parser.parse_args()
-    except SystemExit:
-        parser.print_help()
-        raise
+
+    args = parser.parse_args()
 
     ensure_models_installed()
 
-    if args.serve:
+    if args.mode == MODE_SERVE:
         api.start_api()
         sys.exit(1)
+
+    # we know it is a file input
+    if not args.file:
+        error("Specify a file, you want to parse pls")
+        parser.print_help()
+        sys.exit(0)
 
     # Make path of the input file real and absolute. Ensures cross platform compatibility
     path_to_file = make_path(args.file)
 
-    input_file = open(path_to_file)
-
+    print(path_to_file)
+    input_file = open(path_to_file, encoding='utf-16')
     manual = load_json(input_file)
 
     pipeline.run(manual, visualize=args.visualisation)
